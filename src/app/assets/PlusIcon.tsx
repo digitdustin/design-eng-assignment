f91de009bcc6dda1e783287da7f7257d9860dca2
Dustin Karp <dustinkarp52@gmail.com>
2024-08-28 20:27:02 -0400

    add icons

---
+++ src/app/assets/PlusIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlusIcon = () => {
+  return (
+    <svg className="w-full h-full" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19 12.998H13V18.998H11V12.998H5V10.998H11V4.99799H13V10.998H19V12.998Z" fill="currentColor" />
+    </svg>
+  );
+};
+
+export default PlusIcon;
